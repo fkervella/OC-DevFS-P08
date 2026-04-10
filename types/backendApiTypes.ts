11c1eda918acedc1f062985f677e055d771e19f3
François KERVELLA <kervella.francois@gmail.com>
2026-04-10 13:48:09 +0200

    Ajout des types renvoyés par la backend

--- types/backendApiTypes.ts
+++ types/backendApiTypes.ts
@@ -5,6 +5,12 @@ export interface User {
   role: string;
 }
 
+export interface UpdateUser {
+  name: string;
+  picture: string;
+  role: string;
+}
+
 export interface Property {
   id: string;
   slug: string;
@@ -22,12 +28,84 @@ export interface Property {
   };
 }
 
+export interface UpdateProperty {
+  title: string;
+  description: string;
+  cover: string;
+  location: string;
+  price_per_night: number;
+  host_id: number;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  token: string;
+  user: User;
+}
+
+export interface Rating {
+  id: number;
+  score: number;
+  comment: string;
+  created_at: Date;
+  user: User;
+}
+
+export interface PropertyRating {
+  rating_avg: number;
+  ratings_count: number;
+  ratings: Rating[];
+}
+
+export interface Favorite {
+  id: string;
+  slug: string;
+  title: string;
+  description: string;
+  cover: string;
+  location: string;
+  price_per_night: number;
+  rating_avg: number;
+  ratings_count: number;
+  host: Host;
+}
+
+interface Host {
+  id: number;
+  name: string;
+  picture: string;
+}
+
+export interface Files {
+  url: string;
+  filename: string;
+  size: number;
+  mimetype: string;
+  purpose: string;
+  property_id: string;
+  instructions: string;
+}
+
+export interface RequestResetResponse {
+  ok: boolean;
+  message: string;
+  token: string;
+}
+
+export interface ResetPasswordResponse {
+  ok: boolean;
+}
+
 export type ApiResponses = {
   //Auth
-  login: { data: string };
-  register: { data: string };
-  requestPasswordRequestToken: { data: string };
-  resetPassword: { data: string };
+  login: LoginResponse;
+  register: LoginResponse;
+  requestPasswordRequestToken: RequestResetResponse;
+  resetPassword: ResetPasswordResponse;
 
   //Properties
   properties: Property[];
@@ -38,11 +116,11 @@ export type ApiResponses = {
   user: User;
 
   //Ratings
-  propertyRatings: { data: string };
+  propertyRatings: Rating[];
 
   //Favorites
-  propertyFavorites: { data: string };
-  userFavorites: { data: string };
+  propertyFavorites: PropertyRating;
+  userFavorites: Favorite[];
 
   //Uploads
   image: { data: string };
