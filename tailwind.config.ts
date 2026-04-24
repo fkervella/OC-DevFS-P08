fc0eb75abffbf77e813139a9ef9843a1deb5c8f0
François KERVELLA <kervella.francois@gmail.com>
2026-04-24 16:47:12 +0200

    Modification tailwindcss pour déploiement vercel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,26 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./_components/**/*.{js,ts,jsx,tsx}",
+    "./app/context/**/*.{ts,tsx}",
+    "./hooks/**/*.{ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainRed: "#99331A",
+        grayLight: "#F7F7F7",
+        grayDark: "#4F4F4F",
+        darkOrange: "#842C16",
+        lightOrange: "#FFFBF9",
+        black: "#0D0D0D",
+        white: "#FFFFFF",
+        pinkLight: "#DABFB8",
+        redFont: "#EF4444",
+      },
+    },
   },
   plugins: [],
 };
